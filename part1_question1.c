07d5bed58093ced26df9b1ff5c5a05c293cc138e
Nyalast <Nyala.smith-thompson@stonybrook.edu>
2024-04-04 16:55:17 -0400

    in-lab lab 7 part 1 submission

--- part1_question1.c
+++ part1_question1.c
@@ -1,1 +1,63 @@
+// Nyala Smith-Thompson 115895393
+
+#include <stdio.h>
+
+int my_isalpha (char c){
+    return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
+}
+
+int my_isdigit (char c){
+    return c >= '0' && c <= '9';
+}
+
+int  my_isupper (char c){
+    return c >= 'A' && c <= 'Z';
+}
+
+char my_toupper (char c){
+    if (c >= 'a' && c <= 'z'){
+        return c - 'a' + 'A';
+    }
+    else {
+        return c;
+    }
+}
+
+char my_tolower (char c){
+    if (c >= 'A' && c <= 'Z'){
+        return c - 'A' + 'a';
+    }
+    else {
+        return c;
+    }
+}
+
+int main (){
+    char c;
+
+    printf ("test 1.1 --- is_mydigit:  "); scanf ("%c", &c); getchar();
+    printf ("%d\n", my_isdigit(c));
+    printf ("test 1.2 --- is_mydigit:  "); scanf ("%c", &c); getchar();
+    printf ("%d\n", my_isdigit(c));
+
+    printf ("test 2.1 --- is_myalpha:  "); scanf ("%c", &c); getchar();
+    printf ("%d\n", my_isalpha(c));
+    printf ("test 2.2 --- is_myalpha:  "); scanf ("%c", &c); getchar();
+    printf ("%d\n", my_isalpha(c));
+
+    printf ("test 3.1 --- my_isupper: "); scanf ("%c", &c); getchar();
+    printf ("%d\n", my_isupper(c));
+    printf ("test 3.2 --- my_isupper: "); scanf ("%c", &c); getchar();
+    printf ("%d\n", my_isupper(c));
+    
+    printf ("test 4.1 --- my_toupper: "); scanf ("%c", &c); getchar();
+    printf ("%c\n", my_toupper(c));
+    printf ("test 4.2 --- my_toupper: "); scanf ("%c", &c); getchar();
+    printf ("%c\n", my_toupper(c));
+
+    printf ("test 5.1 --- my_tolower: "); scanf ("%c", &c); getchar();
+    printf ("%c\n", my_tolower(c));
+    printf ("test 5.2 --- my_tolower: "); scanf ("%c", &c); getchar();
+    printf ("%c\n", my_tolower(c));
+}
 
--- part2_question1.c
+++ part2_question1.c
@@ -1,1 +1,101 @@
+#include <stdio.h>
+#include <string.h>
 
+#define MAX 10
+
+char* my_strcpy (char str1[], char str2[]){
+    int i = 0;
+
+    for (i = 0; str2[i] != '\0'; i++){
+        str1[i] = str2[i];
+    }
+
+    return str1;
+}
+
+int my_strcmp(char str1[], char str2[]){ // compares 2 strings and returns 0 if they are equal, otherwise 1
+	int i;
+	
+	for (i = 0; str1[i] != '\0' && str2[i] != '\0'; i++){ // run the loop until one of the string ends
+		if (str1[i] > str2[i] || str1[i] < str2[i]){
+			return 1;
+		}
+	}
+	
+	if (str1[i] == '\0' && str2[i] == '\0'){
+		return 0;
+	}
+    else{
+        return 1;
+    }
+}
+
+char my_strncmp(char str1[], char str2[]){ // compares first n characters of 2 strings and returns 0 if equal, otherwise 1
+    int i, n;
+
+    for (i = 0; str1[i] == '\0' && str2[i] == '\0' && i < n-1; i++){
+        if (str1[i] > str2[i] || str1[i] < str2[i]){
+			return 1;
+		}
+    }
+}
+
+void _strcat(char t[], char s[]){ //appends second string at the end of the first string. returns starting address of first string
+    int t_len = 0;
+
+    while (t[t_len] != '\0'){
+        t_len++;
+    }
+
+    int i = 0;
+    while (s[i] != '\0'){
+        t[t_len++] = s[i++];
+    }
+
+    t[t_len] = '\0';
+}
+
+char my_reverse(char str1[], char str2[]){ //stores reversed second string into first string. returns starting address of first string
+
+}
+
+char MyStrCmp (char str1[], char str2[]){
+	int i;
+	
+	for (i = 0; str1[i] != '\0' && str2[i] != '\0'; i++){ // run the loop until one of the string ends
+		if (str1[i] > str2[i]){
+			return 1;
+		}
+		else if (str1[i] < str2[i]){
+			return -1;
+		}
+	}
+	
+	if (str1[i] == '\0' && str2[i] == '\0'){
+		return 0;
+	}
+	else if (str1[i] == '\0'){
+		return -1;
+	}
+}
+
+int main(){
+    int i;
+    char str1[MAX], str2[MAX];
+
+    printf ("--Copying the second string into the first string and printing result--\n");
+    printf ("Enter first string: "); scanf ("%s", &str1); getchar();
+    printf ("Enter second string: "); scanf ("%s", &str2); getchar();
+    printf ("%s\n", my_strcpy(str1, str2));
+    
+    printf ("--compares 2 strings and returns 0 if they are equal, otherwise 1--\n");
+    printf ("Enter first string: "); scanf ("%s", &str1); getchar();
+    printf ("Enter second string: "); scanf ("%s", &str2); getchar();
+    printf ("%d\n", my_strcmp(str1, str2));
+
+    // printf ("--compares 2 strings and returns 0 if they are equal, otherwise 1--")
+    // printf ("Enter first string: "); scanf ("%s", &str1); getchar();
+    // printf ("Enter second string: "); scanf ("%s", &str2); getchar();
+    // printf ("%s\n", my_strcmp(str1, str2));
+
+}
